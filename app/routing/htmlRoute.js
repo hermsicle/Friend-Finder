98fc3a5881220fbd86c8416b2a145f370073384b
Herman Liu <hermanliu95@gmail.com>
2019-12-19 13:02:07 -0800

    added home.html and survey.html template

---
+++ app/routing/htmlRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+//Connecting the PATH way to our home.html file
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+})
+
+//Connecting the PATH way to our suvery.html file
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public.survey.html"))
+})
+
+
+
+
+module.exports = router;
