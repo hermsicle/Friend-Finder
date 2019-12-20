a8f9f78f720ec3e295ca33b8e64a115a4d710539
Herman Liu <hermanliu95@gmail.com>
2019-12-19 22:34:26 -0800

    created a newUser variable to hold all the data from survey

--- app/public/friends.js
+++ app/public/friends.js
@@ -2,18 +2,26 @@ $(document).ready(function () {
     submitForm();
 })
 
-//Create a function that grabs all the values from input
 submitForm = () => {
     $('#submit').on('click', function () {
         let name = $('#name').val();
         let image = $('#image-link').val();
+        let formData = [];
+        //console.log(formData)
 
         let formVals = $('form').serializeArray()
-        console.log(formVals)
+        for (let i = 0; i < formVals.length; i++) {
+            //console.log(formVals[i].value)
+            formData.push(formVals[i].value)
+        }
 
-        let formVal
+        const newUser = {
+            name: name,
+            photo: image,
+            scores: formData
+        }
+        console.log(newUser)
 
-        console.log(name, image)
     })
 }
 
