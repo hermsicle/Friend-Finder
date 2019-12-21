67030b9f97f1ea9a0ec3c1cb80e46ec1d7dbc864
Herman Liu <hermanliu95@gmail.com>
2019-12-20 16:24:59 -0800

    push

--- app/public/friends.js
+++ app/public/friends.js
@@ -1,28 +1,44 @@
 $(document).ready(function () {
-    submitForm();
+    //submitForm();
 })
 
-submitForm = () => {
-    $('#submit').on('click', function () {
-        let name = $('#name').val();
-        let image = $('#image-link').val();
-        let formData = [];
-        //console.log(formData)
+// $.ajax({
+//     type: 'POST',
+//     url: 'api/friends'
+// }).then(res => {
+//     console.log(res)
+// })
 
-        let formVals = $('form').serializeArray()
-        for (let i = 0; i < formVals.length; i++) {
-            //console.log(formVals[i].value)
-            formData.push(formVals[i].value)
-        }
+//submitForm = () => {
+$('#submit').on('click', function (e) {
+    e.preventDefault();
+    let name = $('#name').val();
+    let image = $('#image-link').val();
+    let formData = [];
+    //console.log(formData)
 
-        const newUser = {
-            name: name,
-            photo: image,
-            scores: formData
-        }
-        console.log(newUser)
+    let formVals = $('form').serializeArray()
+    for (let i = 0; i < formVals.length; i++) {
+        //console.log(formVals[i].value)
+        formData.push(formVals[i].value)
+    }
+
+    const newUser = {
+        name: name,
+        photo: image,
+        scores: formData
+    }
+    //console.log(newUser)
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/friends',
+        data: newUser
+    }).then(res => {
+        console.log(res);
 
     })
-}
+})
+//}
 
 
--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -13,16 +13,22 @@ let friends = [
     new Friend('Jacob Deming', 'https://www.thispersondoesnotexist.com/', ["2", "5", "1", "2", "5", "5", "2", "1", "2", "3"]),
     new Friend('Jeremiah Scanlon', 'https://avatars2.githubusercontent.com/u/8504998?v=3&s=460', ["5", "2", "2", "5", "1", "2", "1", "4", "5", "2"]),
     new Friend('Luiz', 'https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg', ["2", "2", "5", "5", "5", "5", "1", "1", "2", "3"]),
-    new Friend('Thomas', 'https://www.thispersondoesnotexist.com/', ["2", "5", "5", "3", "1", "5", "1", "1", "2", "3"])
+    new Friend('Thomas', 'https://www.thispersondoesnotexist.com/', ["2", "5", "5", "3", "1", "5", "1", "1", "2", "3"]),
+    new Friend('Kandy', 'https://st3.depositphotos.com/14847044/18904/i/1600/depositphotos_189046116-stock-photo-sexy-girl.jpg', [["1", "2", "1", "4", "5", "5", "4", "3", "5", "5"]]),
+    new Friend('Foxy', 'https://st3.depositphotos.com/10131542/16061/i/1600/depositphotos_160613700-stock-photo-young-woman-in-green-bikini.jpg', ["2", "5", "1", "2", "5", "5", "2", "1", "5", "5"])
 ]
 
 //uses the GET method to retrieve data from our API
 router.get('/friends', (req, res) => {
-    console.log('Our Server has been hit!')
+    // console.log('Our Server has been hit!')
     res.send(friends)
 })
 
-
+router.post('/friends', (req, res) => {
+    //console.log('POST has been hit');
+    friends.push(new Friend(req.body.name, req.body.photo, req.body.scores))
+    res.send(friends)
+})
 
 
 
