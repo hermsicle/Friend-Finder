70c18e724e65017621e4c4f4d043609fa6e3bcbc
Herman Liu <hermanliu95@gmail.com>
2019-12-21 20:44:08 -0800

    made better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 2888;
+const PORT = process.env.port || 2888;
 
 //Middleware:
 app.use(express.urlencoded({ extended: true }));
