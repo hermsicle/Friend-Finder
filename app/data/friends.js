6c92797b03e405d286b545661e5fee5ec53f045a
Herman Liu <hermanliu95@gmail.com>
2019-12-26 15:21:56 -0800

    updated photo links, added modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,11 +6,11 @@ function Friend(name, photo, scores = []) {
 }
 
 let friends = [
-    new Friend('Ahmed', 'https://www.thispersondoesnotexist.com/', [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]),
-    new Friend('Jacob Deming', 'https://www.thispersondoesnotexist.com/', [2, 5, 1, 2, 5, 5, 2, 1, 2, 3]),
-    new Friend('Jeremiah Scanlon', 'https://avatars2.githubusercontent.com/u/8504998?v=3&s=460', [5, 2, 2, 5, 1, 2, 1, 4, 5, 2]),
+    new Friend('Ahmed', 'https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg', [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]),
+    new Friend('Jacob Deming', 'https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg', [2, 5, 1, 2, 5, 5, 2, 1, 2, 3]),
+    new Friend('Jeremiah Scanlon', 'https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg', [5, 2, 2, 5, 1, 2, 1, 4, 5, 2]),
     new Friend('Luiz', 'https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg', [2, 2, 5, 5, 5, 5, 1, 1, 2, 3]),
-    new Friend('Thomas', 'https://www.thispersondoesnotexist.com/', [2, 5, 5, 3, 1, 5, 1, 1, 2, 3]),
+    new Friend('Thomas', 'https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg', [2, 5, 5, 3, 1, 5, 1, 1, 2, 3]),
     new Friend('Kandy', 'https://st3.depositphotos.com/14847044/18904/i/1600/depositphotos_189046116-stock-photo-sexy-girl.jpg', [1, 2, 1, 4, 5, 5, 4, 3, 5, 5]),
     new Friend('Foxy', 'https://st3.depositphotos.com/10131542/16061/i/1600/depositphotos_160613700-stock-photo-young-woman-in-green-bikini.jpg', [2, 5, 1, 2, 5, 5, 2, 1, 5, 5])
 ]
--- app/public/app.js
+++ app/public/app.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
 })
 $('#submit').on('click', function (e) {
     e.preventDefault();
+
+    //Check to see if the form is valid
+    let valid = true;
     let name = $('#name').val();
     let image = $('#image-link').val();
     let formData = [];
@@ -15,21 +18,32 @@ $('#submit').on('click', function (e) {
     }
     // console.log(formData)
 
-    const newUser = {
-        name: name,
-        photo: image,
-        scores: formData
+    if (name === "" || image === "") {
+        valid = false;
+    } else if (formData === "") {
+        valid = false;
     }
-    console.log(newUser)
 
-    $.ajax({
-        type: 'POST',
-        url: '/api/friends',
-        data: newUser
-    }).then(res => {
-        //console.log(res);
+    if (valid === true) {
+        let newUser = {
+            name: name,
+            photo: image,
+            scores: formData
+        };
 
-    })
+        let currentURL = window.location.origin;
+
+        // Ajax call for receiving response after POST req
+        $.post(currentURL + "/api/friends", newUser, function (data) {
+            $("#bestFriend").text(data.friends.name);
+            $("#bestFriendPhoto").attr("src", data.friends.photo);
+            $("#bestFriendModal").modal("toggle");
+        });
+    } else {
+        // If a required field is missing, show alert
+        alert("Survey is incomplete!");
+
+    }
 })
 
 //create a function that shows user information:
